de10d9320b9429f7973cac4fa7a44283b54f6e6c
Eko Kurniadi <eko.kurniadi@majoo.id>
2022-06-20 01:06:24 +0700

    refactor transaction step to atomic design

--- components/organisms/TransactionStep/index.tsx
+++ components/organisms/TransactionStep/index.tsx
@@ -5,7 +5,7 @@ export default function TransactionStep() {
     <section id="feature" className="feature pt-50 pb-50">
       <div className="container-fluid">
         <h2 className="text-4xl fw-bold color-palette-1 text-center mb-30">
-          It’s Really That
+          It's Really That
           <br />
           Easy to Win the Game
         </h2>
