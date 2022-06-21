d73633b066e41c9f6e06df67b860da0c3accc9b9
Eko Kurniadi <eko.kurniadi@majoo.id>
2022-06-22 01:10:30 +0700

    create service and env

--- components/organisms/FeatureGames/index.tsx
+++ components/organisms/FeatureGames/index.tsx
@@ -1,6 +1,18 @@
+import { useCallback, useEffect, useState } from "react";
+import { getFeaturedGame } from "../../../services/player";
 import GameItem from "../../molecules/GameItem";
 
 export default function FeatureGames() {
+  const [gameList, setGameList] = useState([]);
+
+  const getFeaturedGameList = useCallback(async () => {
+    const data = await getFeaturedGame();
+    setGameList(data);
+  }, [getFeaturedGame]);
+
+  useEffect(() => {
+    getFeaturedGameList();
+  }, []);
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -13,31 +25,16 @@ export default function FeatureGames() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          <GameItem
-            title="Super Mechs"
-            category="Mobile"
-            thumbnail="Thumbnail-1"
-          />
-          <GameItem
-            title="Call of Duty: Modern"
-            category="Mobile"
-            thumbnail="Thumbnail-2"
-          />
-          <GameItem
-            title="Mobile Legends"
-            category="Mobile"
-            thumbnail="Thumbnail-3"
-          />
-          <GameItem
-            title="Clash of Clans"
-            category="Mobile"
-            thumbnail="Thumbnail-4"
-          />
-          <GameItem
-            title="Valorant"
-            category="Desktop"
-            thumbnail="Thumbnail-5"
-          />
+          {gameList.map((item) => {
+            return (
+              <GameItem
+                key={item._id}
+                title={item.name}
+                category={item.category.name}
+                thumbnail="Thumbnail-1"
+              />
+            );
+          })}
         </div>
       </div>
     </section>
